7f7afc33e96be4d9bd8ba383d2394b85c483f878
MrYamous <matt.lempereur@gmail.com>
2025-12-28 14:31:09 +0100

    chore: add github workflow to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+
+const isDev = import.meta.env.DEV;
 
 // https://astro.build/config
 export default defineConfig({
+  site: isDev ? 'http://localhost:3000' : 'https://mryamous.github.io/portfolio/',
+  base: isDev ? '/' : '/portfolio/',
   vite: {
     plugins: [tailwindcss()]
   }
